2f218f939b084e64129ca108cf6918714d82202c
steveruu <prchalek@gmail.com>
2022-11-11 19:52:27 +0100

    uhhhhhhh i understand nothing

--- react_trash/src/App.js
+++ react_trash/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './TodoList'
 
 function App() {
+  const [todos, setTodos] = useState(['todo 1', 'todo 2'])
   return (
-    <TodoList />
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
+    </>
   );
 }
 
